9fd44c48c0f3e380432fab4c4502a4bd6624da05
Anya Negoda <anyanegoda>
2019-05-14 21:10:43 +0300

    #Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 # User.create(email: 'negoda.anya@gmail.com',password: 'password', admin: true)
 User.create(email: "negoda.anya@gmail.com", password: "password", password_confirmation: "password", admin: true)
+
+NameWork.create([{ name: 'Учебно-методическая работа'},
+             { name: 'Организационно-методическая работа'},
+             {name: 'Научно-исследовательская работа'},
+             {name: 'Воспитательная работа'}])
+
+
 Work.create([{ work_title: 'Подготовка к лекции (если учебная дисциплина преподается впервые)', time_rate: 2, note: 'на 1 час занятий', name_work_id: 1 },
              { work_title: 'Подготовка к практическим (семинарским, лабораторным) занятиям (если учебная дисциплина преподается впервые)', time_rate: 1, note: 'на 1 час занятий', name_work_id: 1 }])
