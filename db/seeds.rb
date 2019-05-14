cdbb2dcb37a0afd7cca617d9d330fe20a95a6802
Anya Negoda <anyanegoda>
2019-05-14 21:35:57 +0300

    #Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,13 @@ NameWork.create([{ name: 'Учебно-методическая работа'},
 
 Work.create([{ work_title: 'Подготовка к лекции (если учебная дисциплина преподается впервые)', time_rate: 2, note: 'на 1 час занятий', name_work_id: 1 },
              { work_title: 'Подготовка к практическим (семинарским, лабораторным) занятиям (если учебная дисциплина преподается впервые)', time_rate: 1, note: 'на 1 час занятий', name_work_id: 1 }])
+
+Setting.create(discipline_full: 'Дисциплина', discipline_extramural: 'Дисциплина', training_direction_full: 'Направление подготовки', training_direction_extramural: 'Направление подготовки',
+                course_full: 'Курс', course_extramural: 'Курс', contingent_full: 'Контингент', contingent_extramural: 'Конт',
+                subgroups_full: 'Количество подгрупп', subgroups_extramural: 'Кол.подгр', semester_full: 'Семестр', lectures_full: 'Лекций', lectures_extramural: 'Лекций',
+                practical_classes_full: 'Практических занятий', practical_classes_extramural: 'Практических занятий ', laboratory_classes_full: 'Лабораторных занятий', laboratory_classes_extramural: 'Лабораторных занятий',
+                consultation_full: 'Консультации', consultation_extramural: 'Консультации', exam_extramural: 'Экзамен', test_plan_full: 'Зачет', test_plan_extramural: 'Зачёт',
+                exam_full: 'Экзамен', exam_v_extramural: 'Экзамен усн', exam_w_extramural: 'Экзамен письм', work_plan_full: 'Предусмотрено рабочим учебным планом', work_plan_extramural: 'Предусмотрено рабочим учебным планом',
+                budget_hours_full: 'Расчет часов (бюджет)', budget_hours_extramural: 'Расчёт часов (бюджет)', contract_hours_full: 'Расчет часов (договор)', contract_hours_extramural: 'Расчёт часов (договор)',
+                department: 'КТ', modular_control_full: 'МК', test_hours_full: 'Зачет', test_hours_extramural: 'Зачет', years: '2018 / 2019'
+               )
